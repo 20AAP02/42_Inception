70a8004d285b37bc931c5ca899cae6dd91a2a02e
antonio-42VM <antoniomariabranches@gmail.com>
2023-10-06 13:05:17 +0100

    trying to go back in weird ways

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# This script ensures that necessary directories are created, 
-# initializes MariaDB if needed, and sets up the specified MySQL user 
-# and database. Finally, it starts the MariaDB server.
-
-# If directory doesn't exist,
-# create and set ownership to the mysql user and group
 if [ ! -d "/run/mysqld" ]; then
 
 	mkdir -p /run/mysqld
@@ -13,8 +7,6 @@ if [ ! -d "/run/mysqld" ]; then
 
 fi
 
-# If MariaDB data directoy doesn't exist, initialize the MariaDB data directory,
-# perform a bootstrap operation and set up the specified MySQL user and database.
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
 	mysql_install_db --user=mysql --skip-test-db --basedir=/usr --datadir=/var/lib/mysql
@@ -31,5 +23,4 @@ EOF
 
 fi
 
-# start the MariaDB server
 mysqld -u mysql

e8afdf54ad7aa6857944ac451d12afd3233e5952
antonio-42VM <antoniomariabranches@gmail.com>
2023-10-06 12:18:51 +0100

    ola

--- srcs/requirements/mariadb/setup.sh
+++ srcs/requirements/mariadb/setup.sh
@@ -4,23 +4,21 @@
 # initializes MariaDB if needed, and sets up the specified MySQL user 
 # and database. Finally, it starts the MariaDB server.
 
-# If directory doesn't exist
+# If directory doesn't exist,
+# create and set ownership to the mysql user and group
 if [ ! -d "/run/mysqld" ]; then
 
-	# create it
 	mkdir -p /run/mysqld
-	# set ownership to the mysql user and group
 	chown -R mysql:mysql /run/mysqld
 
 fi
 
-# If MariaDB data directoy doesn't exist
+# If MariaDB data directoy doesn't exist, initialize the MariaDB data directory,
+# perform a bootstrap operation and set up the specified MySQL user and database.
 if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
-	# initialize the MariaDB data directory
 	mysql_install_db --user=mysql --skip-test-db --basedir=/usr --datadir=/var/lib/mysql
 
-	# perform a bootstrap operation and sets up the specified MySQL user and database.
 	mysqld -u mysql --bootstrap <<EOF
 		flush privileges;
 		create user '${MYSQL_USER}'@'%' identified by '${MYSQL_PASSWORD}';
